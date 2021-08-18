1b2e64368a1aaaf12c7cf5115c91b7d5a648bd32
cole <colehorvitz@gmail.com>
2021-08-17 23:54:10 -0700

    added green bar

--- src/ItemDisplay.js
+++ src/ItemDisplay.js
@@ -25,62 +25,69 @@ class ItemDisplay extends Component {
 
   renderProcessGrid = () => {
     if (this.props.item.processImages.length > 0) {
-      return this.props.item.processImages.map((img) => <div>
-        <img  onClick={() =>
-              this.props.setOverlayImage(
-                "photos/" + img
-              )} src={"photos/" +  img} />
-        </div>)
+      return this.props.item.processImages.map((img) => (
+        <div>
+          <img
+            onClick={() => this.props.setOverlayImage("photos/" + img)}
+            src={"photos/" + img}
+          />
+        </div>
+      ));
     } else {
-      return <div></div>
+      return <div></div>;
     }
-  }
+  };
 
   render() {
     return (
       <div className="display-wrap">
-        <h2 className="display-title">{this.props.item.title}</h2>
-        {this.props.item.details.map((detail) => (
-          <p className="display-detail">{detail}</p>
-        ))}
-        <div className="display-image-wrap">
-          <div className="arrow-wrap">
-            <MdChevronLeft
-              className={
-                this.state.carouselIndex === 0 ? "icon-disabled" : "icon"
+        <div className="display-content">
+          <h2 className="display-title">{this.props.item.title}</h2>
+          {this.props.item.details.map((detail) => (
+            <p className="display-detail">{detail}</p>
+          ))}
+          <div className="display-image-wrap">
+            <div className="arrow-wrap">
+              <MdChevronLeft
+                className={
+                  this.state.carouselIndex === 0 ? "icon-disabled" : "icon"
+                }
+                onClick={() => this.indexHandler(-1)}
+              />
+            </div>
+            <img
+              className="display-image"
+              src={"photos/" + this.props.item.images[this.state.carouselIndex]}
+              alt={this.props.title}
+              onClick={() =>
+                this.props.setOverlayImage(
+                  "photos/" + this.props.item.images[this.state.carouselIndex]
+                )
               }
-              onClick={() => this.indexHandler(-1)}
-            />
+            />{" "}
+            <div className="arrow-wrap">
+              <MdChevronRight
+                className={
+                  this.state.carouselIndex === this.props.item.images.length - 1
+                    ? "icon-disabled"
+                    : "icon"
+                }
+                onClick={() => this.indexHandler(1)}
+              />
+            </div>
           </div>
-          <img
-            className="display-image"
-            src={"photos/" + this.props.item.images[this.state.carouselIndex]}
-            alt={this.props.title}
-            onClick={() =>
-              this.props.setOverlayImage(
-                "photos/" + this.props.item.images[this.state.carouselIndex]
-              )
-            }
-          />{" "}
-          <div className="arrow-wrap">
-            <MdChevronRight
-              className={
-                this.state.carouselIndex === this.props.item.images.length - 1
-                  ? "icon-disabled"
-                  : "icon"
-              }
-              onClick={() => this.indexHandler(1)}
-            />
+          <div className="display-description">
+            <h3 className="display-description-title">Description:</h3>
+            <p className="display-detail">{this.props.item.description}</p>
           </div>
+          {this.props.item.processImages.length > 0 ? (
+            <h3 className="display-description-title">Process:</h3>
+          ) : (
+            <div />
+          )}
+          <div className="process-grid">{this.renderProcessGrid()}</div>
         </div>
-        <div className="display-description">
-          <h3 className="display-description-title">Description:</h3>
-          <p className="display-detail">{this.props.item.description}</p>
-        </div>
-        {this.props.item.processImages.length > 0 ? <h3 className="display-description-title">Process:</h3> : <div/>}
-        <div className="process-grid">
-          {this.renderProcessGrid()}
-        </div>
+        <div className="footer-border"></div>
       </div>
     );
   }
