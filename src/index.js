ada20d414dafed4ec7c509be79f31e472228ad52
cole <colehorvitz@gmail.com>
2021-08-19 21:26:31 -0700

    gitignore

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './fonts/neue-haas-grotesk/NeueHaasGrotDisp-45Light.otf'
+// import './fonts/neue-haas-grotesk/NeueHaasGrotDisp-45Light.otf'
 
 ReactDOM.render(
   <React.StrictMode>
